9b673702bbd2adc1da5617f570a2791b57e438ae
kylib4444 <abilyk.crytek@gmail.com>
2026-06-28 01:27:15 +0000

    fix: ignore unused request param in metrics

--- app/server/index.ts
+++ app/server/index.ts
@@ -15,7 +15,7 @@ import { register } from 'prom-client';
 
 const app = express();
 
-app.get('/metrics', async (req, res) => {
+app.get('/metrics', async (_req, res) => { 
   try {
     res.set('Content-Type', register.contentType);
     res.send(await register.metrics());
